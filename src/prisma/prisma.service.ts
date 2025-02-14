5345d0e614bc830a0ad3c4472ad449308efe5495
MahdiSaljoughi <saljooghimahdii@gmail.com>
2025-02-14 07:26:28 +0330

    fix

--- src/prisma/prisma.service.ts
+++ src/prisma/prisma.service.ts
@@ -1,15 +1,18 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, OnModuleInit } from '@nestjs/common';
 import { PrismaClient } from '@prisma/client';
 
 @Injectable()
-export class PrismaService extends PrismaClient {
-  constructor() {
-    super({
-      datasources: {
-        db: {
-          url: process.env.DATABASE_URL,
-        },
-      },
-    });
+export class PrismaService extends PrismaClient implements OnModuleInit {
+  async onModuleInit() {
+    await this.$connect();
   }
+  // constructor() {
+  //   super({
+  //     datasources: {
+  //       db: {
+  //         url: process.env.DATABASE_URL,
+  //       },
+  //     },
+  //   });
+  // }
 }
