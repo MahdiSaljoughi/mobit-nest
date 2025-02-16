e4227e611a16cf569ec39f5d58e6996893be6899
MahdiSaljoughi <saljooghimahdii@gmail.com>
2025-02-16 15:31:10 +0330

    add cors orgin netlify

--- src/main.ts
+++ src/main.ts
@@ -5,10 +5,15 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   app.enableCors({
-    origin: ['http://localhost:3000', 'https://moobit.vercel.app'],
+    origin: [
+      'http://localhost:3000',
+      'https://moobit.vercel.app',
+      'https://moobit.netlify.app',
+    ],
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
   });
 
   await app.listen(process.env.NEST_PORT ?? 5555);
 }
+// eslint-disable-next-line @typescript-eslint/no-floating-promises
 bootstrap();
