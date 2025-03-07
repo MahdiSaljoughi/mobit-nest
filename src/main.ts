6564a4472a0aaafdd746b9b6eb953f8e84b4694f
Mahdi <saljooghimahdii@gmail.com>
2025-03-07 07:41:56 +0330

    docs

--- src/main.ts
+++ src/main.ts
@@ -1,17 +1,24 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   app.enableCors({
-    origin: [
-      'http://localhost:3000',
-      'https://moobit.vercel.app',
-      'https://moobit.netlify.app',
-    ],
+    origin: ['http://localhost:3000', 'https://moobit.vercel.app'],
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
   });
+
+  const config = new DocumentBuilder()
+    .setTitle('API Documentation')
+    .setDescription('This is the API documentation for our mobit clone.')
+    .setVersion('1.0')
+    .addBearerAuth()
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api-docs', app, document);
 
   await app.listen(process.env.NEST_PORT ?? 5555);
 }
